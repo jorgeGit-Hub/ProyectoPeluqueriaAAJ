62f40f0793b844dc3e9658df666f5138fb40afda
NaboVolador <aleixmingot06@gmail.com>
2026-02-25 09:43:14 +0100

    App finalizada de C#

--- PeluqueriaApp_PRO_UI/Models/Models.cs
+++ PeluqueriaApp_PRO_UI/Models/Models.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
-
+//comentario prueba
 namespace PeluqueriaApp.Models
 {
     // ========== AUTENTICACIÓN ==========
